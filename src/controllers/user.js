b2f6bc88c6cd4f34dad7021530cc9a5a8ef3b4c9
romain <rom1ain92@gmail.com>
2020-10-13 14:17:27 +0200

    commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,25 +5,28 @@ module.exports = {
         // Check parameters
         if (!user.username)
             return callback(new Error("Wrong user parameters"), null)
-        // Create User schema
+                // Create User schema
         const userObj = {
-            firstname: user.firstname,
-            lastname: user.lastname,
-        }
-        // Save to DB
-        // TODO check if user already exists
-        if (!res) {
-            client.hmset(user.username, userObj, (err, res) => {
-                if (err) return callback(err, null)
-                callback(null, res) // Return callback
-            })
-        }
+                firstname: user.firstname,
+                lastname: user.lastname,
+            }
+            // Save to DB
+            // TODO check if user already exists
+        client.hmget(user.username, (err, res) => {
+            if (!res) {
+                client.hmset(user.username, userObj, (err, res) => {
+                    if (err) return callback(err, null)
+                    callback(null, res) // Return callback
+                })
+            }
+            return callback(new Error("User already exists"), null)
 
+        })
     },
     // get: (username, callback) => {
     //   // TODO create this method
     // }
-    get: function (username, callback) {
+    get: function(username, callback) {
 
         // Get from DB
         client.hmget(username, ["firstname", "lastname"], (err, res) => {
